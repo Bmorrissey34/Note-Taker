50623641f9569aec51ec4039f42d60a356c3d55a
Bmorrissey34 <88950762+Bmorrissey34@users.noreply.github.com>
2022-01-12 18:30:22 -0500

    working

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Note-Taker
+
+## Description 
+
+This app was created to use everyday! You can save new notes and edit ones you have already created. 
+
+## Installation 
+
+- Npm install in the correct folders
+- Run Node server.js in the terminal
+- Once you get the confirmation message open your browser to http://localhost:3001/
+
+## Usage 
+
+Github Repo: https://github.com/Bmorrissey34/Note-Taker
+
--- server.js
+++ server.js
@@ -1,114 +1,121 @@
 //build standard express app
 
 //routes
-    //html routes
-    //home route
-        //send index.html
-    //notes route
-        //send notes.html
-    
+//html routes
+//home route
+//send index.html
+//notes route
+//send notes.html
+
 //API ROUTES
-    //get api/notes
-        //read db.json (need fs)
-        //parse data into json
-        //send json to front end
-        //if this is working you will see notes in the app
-    //POST /api/notes
-        //make bare bones post /api/notes route
-        //try console.log(req.body)
-        //make new object with text and title keys
-        //tack on ID (id) to that new object
-        //read the file db.json -> parse into json -> push the new note onto array -> stringify data -> fs.writeFile
+//get api/notes
+//read db.json (need fs)
+//parse data into json
+//send json to front end
+//if this is working you will see notes in the app
+//POST /api/notes
+//make bare bones post /api/notes route
+//try console.log(req.body)
+//make new object with text and title keys
+//tack on ID (id) to that new object
+//read the file db.json -> parse into json -> push the new note onto array -> stringify data -> fs.writeFile
 
-    //Bonus DELETE /api/note/:id
-        //read the db.json -> 
-        //parse into JSON -> 
-        //find the note whose id matches the req param id -> 
-        //stringify data -> 
-        //fs.writeFile
+//Bonus DELETE /api/note/:id
+//read the db.json -> 
+//parse into JSON -> 
+//find the note whose id matches the req param id -> 
+//stringify data -> 
+//fs.writeFile
 
-        const fs = require('fs')
-        const path = require('path');
-        const api = require('./routes/index.js');
-        const notes = require('./db/db.json');
-        const { uuid } = require('uuidv4')
-        const express = require('express');
-        const PORT = process.env.PORT || 3001;
-        
-        const app = express();
-        
-        
-        // Middleware for parsing JSON and urlencoded form data
-        app.use(express.json());
-        app.use(express.urlencoded({ extended: true }));
-        app.use('/api', api);
-        
-        app.use(express.static('public'));
-        
-        
-        app.get('/', (req, res) =>
-          res.sendFile(path.join(__dirname, '/public/index.html'))
-        );
-        
-        app.get('/notes', (req, res) =>
-          res.sendFile(path.join(__dirname, '/public/notes.html'))
-        );
-        
-        app.get('/api/notes', (req, res) => {
-        fs.readFile('./db/db.json', 'utf8', (err, data) => {
-          if (err) {
-            console.error(err)  
-          } else {
+const fs = require('fs')
+const path = require('path');
+const api = require('./routes/index.js');
+const notes = require('./db/db.json');
+const {
+    uuid
+} = require('uuidv4')
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err)
+        } else {
             res.json(JSON.parse(data))
-          }
-            
-        })});
-        
-        app.post('/api/notes', (req, res) => {
-          console.info(`${req.method} request received to add a note`);
-        
-        
-          const { title, text} = req.body;
-        
-         
-          if (title && text) {
-           
-            const newNote = {
-              title,
-              text,
-              id: uuid()
-            };
-        
-            const response = {
-              status: 'success',
-              body: newNote,
-            };
-            
-            fs.readFile('./db/db.json', 'utf8', (err, data) => {
-              if (err) {
+        }
+    })
+});
+
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+
+
+    const {
+        title,
+        text
+    } = req.body;
+
+
+    if (title && text) {
+
+        const newNote = {
+            title,
+            text,
+            id: uuid()
+        };
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
                 console.error(err);
-              } else {
+            } else {
                 const PRS = JSON.parse(data);
                 PRS.push(newNote);
                 fs.writeFile('./db/db.json', JSON.stringify(PRS), (err) => {
-                  if (err) {
-                    console.error(err)
-                  } else {
-                    res.json('Success')
-                  }
+                    if (err) {
+                        console.error(err)
+                    } else {
+                        res.json('Success')
+                    }
                 })
-              }
-            });
-            console.log('success')
-          } else {
-            res.status(500).json('Error in posting review');
-          }
+            }
         });
-        
-        app.get('*', (req, res) =>
-          res.sendFile(path.join(__dirname, 'public/index.html'))
-        );
-        
-        app.listen(PORT, () =>
-          console.log(`App listening at http://localhost:${PORT} 🚀`)
-        );
+        console.log('success')
+    } else {
+        res.status(500).json('Error in posting review');
+    }
+});
+
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
